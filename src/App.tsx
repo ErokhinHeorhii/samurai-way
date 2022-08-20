514a014b5523731ed8ce2acc31c3b9a0c726c1c2
George Erokhin <Georgii070684@gmail.com>
2022-08-20 14:23:41 +0300

    feat: add 19 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,29 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
 import s from "./components/Dialogs/Dialogs.module.css";
+import { BrowserRouter,  Route } from "react-router-dom";
+import News from './Router/News/News';
+import Music from './Router/Music/Music';
+import Setting from './Router/Setting/Setting';
+
 
 function App(props: any) {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            {/* <Profile/>*/}
-            <div className='app-wrapper-content'>
-                <Dialogs/>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header />
+                <Navbar />
+                <div className='app-wrapper-content'>
+                  
+                        <Route path='/dialogs' component={Dialogs} />
+                        <Route path='/profile' component={Profile} />
+                        <Route path='/news' component={News} />
+                        <Route path='/music' component={Music} />
+                        <Route path='/setting' component={Setting} />
+                  
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
