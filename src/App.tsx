f4d01912dc9d670db9f3df24920a5a3e5953f275
George Erokhin <Georgii070684@gmail.com>
2022-08-20 12:20:26 +0300

    feat: add 18 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './Profile/Profile';
+import Dialogs from "./components/Dialogs/Dialogs";
+import s from "./components/Dialogs/Dialogs.module.css";
 
-function App() {
-  return (
-    <div className="app-wrapper">
-      <Header/>
-      <Navbar/>
-      <Profile/>
-    </div>
-  );
+function App(props: any) {
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar/>
+            {/* <Profile/>*/}
+            <div className='app-wrapper-content'>
+                <Dialogs/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/Profile/Profile.tsx
+++ src/Profile/Profile.tsx
@@ -2,7 +2,7 @@ import MyPost from './MyPost/MyPost'
 import s from './Profile.module.css'
 
 const Profile = () => {
-  return (<div className={`${s.content} ${s.active}`}>
+  return (<div >
     <div>
       <img
         className={s.Image}
@@ -13,6 +13,7 @@ const Profile = () => {
       + discription
     </div>
     < MyPost />
+
   </div>)
 }
 export default Profile
