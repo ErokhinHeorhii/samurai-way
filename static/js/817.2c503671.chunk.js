1b6e6bea179cfa97023affe9cc3a9f662fb991b4
George Erokhin <Georgii070684@gmail.com>
2023-01-03 20:46:51 +0300

    Updates

---
+++ static/js/817.2c503671.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksamurai_way=self.webpackChunksamurai_way||[]).push([[817],{9817:function(a,u,e){e.r(u);var s=e(184);u.default=function(){return(0,s.jsx)("div",{children:"News"})}}}]);
+//# sourceMappingURL=817.2c503671.chunk.js.map
